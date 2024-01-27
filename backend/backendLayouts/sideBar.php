e04a6d9211682d47c44f3a65489b25dc68d80b17
Shahidul-Tanim <115738942+Shahidul-Tanim@users.noreply.github.com>
2024-01-27 22:28:48 +0600

    sideBar.php

--- backend/backendLayouts/sideBar.php
+++ backend/backendLayouts/sideBar.php
@@ -23,22 +23,20 @@
 <hr class="sidebar-divider">
 
 <!-- Heading -->
-<div class="sidebar-heading">
-    Interface
-</div>
+
 
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
+        <span>Banners</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            
+            <a class="collapse-item" href="./addBanners.php">Add Banner</a>
+            <a class="collapse-item" href="./allBanners.php">All Banners</a>
         </div>
     </div>
 </li>
