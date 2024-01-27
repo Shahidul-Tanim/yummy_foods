8ca8f1e3bb9d07774bf7ec0ef3f81b1cce0a59b2
Shahidul-Tanim <115738942+Shahidul-Tanim@users.noreply.github.com>
2024-01-27 22:27:29 +0600

    header.php

--- backend/backendLayouts/header.php
+++ backend/backendLayouts/header.php
@@ -1,3 +1,25 @@
+<?php
+session_start();
+
+// *IF USER AUTH LOGGED IN
+if(!isset($_SESSION['auth'])){
+    session_unset();
+    header("Location: ./login.php");
+}
+
+
+// *Profile Function
+
+function getProfileImage(){
+    if(isset($_SESSION['auth']['profile_img'])){
+        return "../uploads/" . $_SESSION['auth']['profile_img'];
+    }
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -209,14 +231,14 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= ucwords($_SESSION['auth']['fname'] )?? '' ?></span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="<?= getProfileImage()?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="./profile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
